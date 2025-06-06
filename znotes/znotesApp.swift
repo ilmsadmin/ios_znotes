6be14afeba2dda34b29689f6df5afff2c394804f
adminilms <admin@ilms.vn>
2025-06-06 23:09:34 +0700

    Initial Commit

---
+++ znotes/znotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  znotesApp.swift
+//  znotes
+//
+//  Created by toan on 6/6/25.
+//
+
+import SwiftUI
+
+@main
+struct znotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ znotesTests/znotesTests.swift
@@ -0,0 +1,17 @@
+//
+//  znotesTests.swift
+//  znotesTests
+//
+//  Created by toan on 6/6/25.
+//
+
+import Testing
+@testable import znotes
+
+struct znotesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
